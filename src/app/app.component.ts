e6f3d52e9b4343cd49cfa1d52847c4c36295d39c
yuichi <20w2100d@shinshu-u.ac.jp>
2021-04-10 12:39:29 +0900

    heroコンポーネント追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'b4kadai';
+  title = 'NAGANO';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
